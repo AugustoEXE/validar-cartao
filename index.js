b357ea24e07d0bd0db8fac55a05394fab5a2991b
AugustoEXE <augusto.cr2005@gmail.com>
2023-04-24 00:04:16 -0300

    adnawe

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  const { token, data } = req.query;
+
+  const uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  if (uuidRegex.test(token)) {
+    res.send(`Cartão valido para o aluno: ${data} `);
+  } else {
+    // Token inválido
+    res.send("Token inválido");
+  }
 });
 
 app.listen(3001, () => {
-  console.log(`Example app listening at http://localhost:3001`);
+  console.log("Servidor iniciado na porta 3000");
 });
